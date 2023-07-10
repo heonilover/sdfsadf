203604393b047ae4a889aafd47ef168ce1a6dffa
heonilover <pespor2@gmail.com>
2023-07-10 23:18:51 +0900

    dfsfdsfsdf

--- DB/index.js
+++ DB/index.js
@@ -11,6 +11,8 @@ mongoose.connect("mongodb://127.0.0.1:27017");
 app.listen(80);
 
 app.get('/', async function(req, res){
+    const title=req.params.title
+    const content=req.params.content
     res.json(await model.find({title, content}));
 });
 
